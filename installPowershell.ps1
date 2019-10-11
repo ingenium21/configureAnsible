8ca7b56b0daa6691618e5e29e63c99e4096ef7dd
Renato Regalado <renato.regalado@gmail.com>
2019-10-11 16:02:58 -0500

    fixed missing curly bracket

--- installPowershell.ps1
+++ installPowershell.ps1
@@ -3,3 +3,4 @@ $servers = get-content .\servers.txt
 
 foreach ($s in $servers){
     .\psexec \\$s -accepteula -nobanner -s powershell -inputFormat none -executionpolicy bypass -Command "choco install dotnet4.5 -y; choco install powershell -y"
+}
